05f0eb1476efdeb9051d7e0ec3647a560562421f
Mikhail Kidyankin <kidyankin@icloud.com>
2020-02-26 22:51:29 +0300

    Add submodule update for CircleCI

--- Repo/build.sh
+++ Repo/build.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-git submodule init 
-git submodule update --remote
-
 DIR=${1:-"."}
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 echo "Build local docker image for branch $BRANCH"
