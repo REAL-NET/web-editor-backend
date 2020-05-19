0684f1fc575803ab714a2f529563e4086d32e745
Mikhail Kidyankin <kidyankin@icloud.com>
2020-05-19 13:53:26 +0300

    Fix serialization for abstract elements

--- Repo/RepoAPI/AutomapperProfile.cs
+++ Repo/RepoAPI/AutomapperProfile.cs
@@ -13,9 +13,15 @@ namespace RepoAPI
             CreateMap<Repo.AttributeKind, Models.AttributeKind>().ReverseMap();
             CreateMap<IAttribute, Attribute>().ReverseMap();
             CreateMap<IElement, ElementInfo>();
-            CreateMap<IElement, Element>();
-            CreateMap<INode, Node>();
-            CreateMap<IEdge, Edge>();
+            CreateMap<IElement, Element>()
+                .ForMember(x => x.InstanceMetatype, x => x.MapFrom(y => y.IsAbstract ? 0 : y.InstanceMetatype))
+                .ForMember(x => x.Shape, x => x.MapFrom(y => y.IsAbstract ? null : y.Shape));
+            CreateMap<INode, Node>()
+                .ForMember(x => x.InstanceMetatype, x => x.MapFrom(y => y.IsAbstract ? 0 : y.InstanceMetatype))
+                .ForMember(x => x.Shape, x => x.MapFrom(y => y.IsAbstract ? null : y.Shape));
+            CreateMap<IEdge, Edge>()
+                .ForMember(x => x.InstanceMetatype, x => x.MapFrom(y => y.IsAbstract ? 0 : y.InstanceMetatype))
+                .ForMember(x => x.Shape, x => x.MapFrom(y => y.IsAbstract ? null : y.Shape));
             CreateMap<IModel, Model>()
                 .ForMember(x => x.MetamodelName, x => x.MapFrom(y => y.Metamodel.Name));
         }
